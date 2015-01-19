93d71914ba46d398e53f085e25f65c29926e1727
Iuro Nascimento <iurobpn@gmail.com>
2015-01-19 21:24:21 -0200

    date and time on filename feature implemented

--- dasl.py
+++ dasl.py
@@ -5,20 +5,23 @@
 #file: dasl.py
 #Author: Iuro Nascimento
 #Date(dd/mm/yyyy): 14/01/2015
+# Finished on 19/01/2015
 
 import sys
 import serial
 import signal
 import argparse
 import struct
+from datetime import datetime, time, date
 #from servo import process
 
 #parsing of command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--serialport", type=str, help="(default=/dev/ttyACM0)",default="/dev/ttyACM0")
+parser = argparse.ArgumentParser(description="Log serial data received with the format |0xFFFF | lenght(1 byte) | checksum(1 byte) | into a binary file with the format: | data_size(in bytes, 4bytes) | raw_binary_data |. The purpose of this script is to log data from microcontrollers with in a more secure way than just throwing data over the serial port and reading on the computer with any verification whatsoever.")
+parser.add_argument("-p", "--serialport", type=str, help="(default=/dev/ttyUSB0)",default="/dev/ttyUSB0")
 parser.add_argument("-n", "--data_size", type=int, help="the number of data 'points'to be received(default=0, no limite, hit Ctrl+c to quit and save the data to file)",default=20)
-parser.add_argument("-f", "--output_file", type=str, help="name of the binary data file to be created(default=data.bin)",default="data.bin")
+parser.add_argument("-f", "--output_file", type=str, help="name of the binary data file to be created(default=data.bin)",default="data")
 parser.add_argument("-b", "--baudrate", type=int,help="(default=115200)",default=115200)
+parser.add_argument("-d", "--datetime", help="turn off the date, time and .bin extension at the and of filename",action='store_true')
 args=parser.parse_args()
 
 #global variables
@@ -26,10 +29,23 @@ baud_rate=args.baudrate
 outfile=args.output_file
 data_size=args.data_size
 port=args.serialport
+dtime=args.datetime
 data_list=[]
 pack_size=0
 
 def save_data():
+  global outfile
+  if not dtime:
+    current_date=datetime.today()
+    hour=current_date.hour
+    minute=current_date.minute
+    day=current_date.day
+    month=current_date.month
+    year=current_date.year
+    outfile='.'.join([outfile,str(year),str(month),str(day)]) + '_' + ':'.join([str(hour),str(minute)]) + '.bin'
+  if outfile == "data":
+    outfile +='.bin'
+
   binfile = open(outfile, 'wb')
   binfile.write(struct.pack('i',len(data_list)))
   #binfile.write(struct.pack('i',pack_size))
@@ -94,7 +110,6 @@ def receive_data():
   i=0
 
   #open the data file
-  log = open(outfile, 'wb')
   while (data_size==0) or (i<data_size):
     #verify 
     num_bytes=0;
@@ -131,7 +146,6 @@ def receive_data():
       receive_data.last=buffer#at this point, buffer is a single byte, and may or may not be matched one of the header bytes
       #print('not a package')
   ser.close()
-  log.close()
 
 
 def main():
